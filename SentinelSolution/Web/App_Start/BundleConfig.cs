485e16bf6addb2ece4bc52dc26682d6df080c99d
gybalassy <balassy@avalon.aut.bme.hu>
2013-11-07 06:36:44 +0100

    Static folder.

--- SentinelSolution/Web/App_Start/BundleConfig.cs
+++ SentinelSolution/Web/App_Start/BundleConfig.cs
@@ -1,31 +1,31 @@
-﻿using System.Web;
-using System.Web.Optimization;
-
-namespace Sentinel.Web
+﻿namespace Sentinel.Web
 {
+	using System.Web.Optimization;
+
+
 	public class BundleConfig
 	{
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles( BundleCollection bundles )
 		{
 			bundles.Add( new ScriptBundle( "~/bundles/jquery" ).Include(
-									"~/Scripts/jquery-{version}.js" ) );
+									"~/Static/lib/jquery-{version}.js" ) );
 
 			bundles.Add( new ScriptBundle( "~/bundles/jqueryval" ).Include(
-									"~/Scripts/jquery.validate*" ) );
+									"~/Static/lib/jquery.validate*" ) );
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add( new ScriptBundle( "~/bundles/modernizr" ).Include(
-									"~/Scripts/modernizr-*" ) );
+									"~/Static/lib/modernizr-*" ) );
 
 			bundles.Add( new ScriptBundle( "~/bundles/bootstrap" ).Include(
-								"~/Scripts/bootstrap.js",
-								"~/Scripts/respond.js" ) );
+								"~/Static/bootstrap/js/bootstrap.js",
+								"~/Static/bootstrap/js/respond.js" ) );
 
 			bundles.Add( new StyleBundle( "~/Content/css" ).Include(
-								"~/Content/bootstrap.css",
-								"~/Content/site.css" ) );
+								"~/Static/bootstrap/css/bootstrap.css",
+								"~/Static/my.css" ) );
 		}
 	}
 }

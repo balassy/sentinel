3551f335fa447abf906cd0c466993da963378361
gybalassy <balassy@avalon.aut.bme.hu>
2013-11-16 06:21:53 +0100

    Listing the images in a gallery.

--- SentinelSolution/Web/Controllers/GalleryController.cs
+++ SentinelSolution/Web/Controllers/GalleryController.cs
@@ -1,7 +1,9 @@
 ﻿namespace Sentinel.Web.Controllers
 {
+	using System;
 	using System.Collections.Generic;
 	using System.IO;
+	using System.Linq;
 	using System.Web.Mvc;
 	using Sentinel.Web.Models.Gallery;
 
@@ -47,7 +49,46 @@
 
 		public virtual ActionResult ViewGallery( string folderName )
 		{
-			return View();
+			// Input validation: the folder name must be specified.
+			if( String.IsNullOrEmpty( folderName ) )
+			{
+				throw new ArgumentNullException( "folderName" );
+			}
+
+			// Input validation: the folder name must not contain any invalid character.
+			if( folderName.IndexOfAny( Path.GetInvalidPathChars() ) > -1 || folderName.IndexOfAny( Path.GetInvalidFileNameChars() ) > -1 )
+			{
+				throw new ArgumentException( "The folder name contains invalid characters!", "folderName" );
+			}
+
+			string folderVirtualPath = Path.Combine( StorageFolderVirtualPath, folderName );
+			string folderPhysicalPath = this.Server.MapPath( folderVirtualPath );
+
+			// Integrity check: the folder must exist.
+			if( !Directory.Exists( folderPhysicalPath )  )
+			{
+				throw new ArgumentException( "The specified folder does not exist!", "folderName" );
+			}
+
+			// Integrity check: the requested folder must be the direct child of the storage folder.
+			string storagePhysicalPath = this.Server.MapPath( StorageFolderVirtualPath );
+			string folderParentPhysicalPath = Directory.GetParent( folderPhysicalPath ).FullName;
+			if( !folderParentPhysicalPath.Equals( storagePhysicalPath, StringComparison.OrdinalIgnoreCase ) )
+			{
+				throw new ArgumentOutOfRangeException( "Invalid folder name!", "folderName" );
+			}
+
+			ViewGalleryVM model = new ViewGalleryVM
+			{
+				Title = folderName,
+				PhotoUrls = new List<string>()
+			};
+
+			// Enumerate the files in the requested folder.
+			IEnumerable<string> photoUrls = Directory.EnumerateFiles( folderPhysicalPath, "*.jpg" ).Select( filePhysicalPath => Path.Combine( folderVirtualPath, Path.GetFileName( filePhysicalPath ) ) );
+			model.PhotoUrls.AddRange( photoUrls );
+
+			return View( model );
 		}
 
 	}
---
+++ SentinelSolution/Web/Models/Gallery/ViewGalleryVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Sentinel.Web.Models.Gallery
+{
+	using System.Collections.Generic;
+
+
+	public class ViewGalleryVM
+	{
+		public string Title { get; set; }
+
+		public List<string> PhotoUrls { get; set; }		
+	}
+}

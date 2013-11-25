e32b1d58b1d9a0a07b39077666dedb8b5f97b75d
gybalassy <balassy@avalon.aut.bme.hu>
2013-11-25 10:44:56 +0100

    Remove the default version headers for security.

--- SentinelSolution/Web/Global.asax.cs
+++ SentinelSolution/Web/Global.asax.cs
@@ -16,6 +16,17 @@ namespace Sentinel.Web
 			FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
 			RouteConfig.RegisterRoutes( RouteTable.Routes );
 			BundleConfig.RegisterBundles( BundleTable.Bundles );
+
+			// Security: Remove the X-AspNetMVc-Version: 5.0 HTTP header.
+			MvcHandler.DisableMvcResponseHeader = true;
 		}
+
+
+		protected void Application_PreSendRequestHeaders()
+		{
+			// Security: Remove the Server: Microsoft-IIS/8.0 HTTP header.
+			this.Response.Headers.Remove( "Server" );
+		}
+
 	}
 }
